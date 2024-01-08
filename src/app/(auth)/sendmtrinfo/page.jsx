6dbe1d3f90af5cb98995e7189e7e97e5a4ccc3d6
fazekas28 <148578127+fazekas28@users.noreply.github.com>
2024-01-08 15:40:51 -0300

    Create page.jsx

---
+++ src/app/(auth)/sendmtrinfo/page.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import Send_mtr_form from '../../../components/form/Send_mtr_form'
+import { getXataClient } from '../../../lib/xata'
+import { auth } from '@clerk/nextjs';
+import { Toaster } from 'react-hot-toast';
+
+const xata = getXataClient()
+
+export default async function SendMtrInfo() {
+  const { sessionClaims, userId } = auth();
+  const filial_name = sessionClaims?.org_slug
+
+  const getFilialInfo = async (cod) => {
+    'use server'
+    const cod_ibama = parseInt(cod)
+    const filialInfo = await xata.db.filial_info.filter({
+      filial: filial_name,
+      cod_ibama: cod_ibama
+    }).getMany();
+
+    if (!filialInfo) {
+      return "aconteceu algum erro!"
+    } else return filialInfo[0].id
+
+  }
+
+  const saveMtrInfo = async (formData, id) => {
+    'use server'
+
+    const inputDate = new Date(formData.data);
+    const formatedDate = inputDate.toISOString();
+
+    const volumeFloat = parseFloat(formData.volume)
+
+    try {
+      const saveMtrInfo = await xata.db.mtr_info.create({
+        link_id: id,
+        data: formatedDate,
+        volume: volumeFloat,
+        n_mtr: formData.n_mtr,
+        n_cdf: formData.n_cdf,
+        n_nf: formData.n_nf,
+        chave_nf: formData.chave_nf,
+        userId: userId
+      })
+
+
+    } catch (error) {
+      return 'Não foi possível salvar as informações!'
+    }
+    return 'Informações salvas com sucesso!'
+
+  }
+
+
+
+
+  return (
+    <div className='flex flex-col pl-[4.25em] pt-10 gap-5'>
+      <Send_mtr_form getFilialInfo={getFilialInfo} saveMtrInfo={saveMtrInfo} />
+      <Toaster
+        toastOptions={{
+          duration: 4000
+        }} />
+    </div>
+  )
+}
