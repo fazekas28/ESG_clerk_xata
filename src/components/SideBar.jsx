fc5d4f96a64d4ce0c969e7f644d26e2849a80a16
fazekas28 <148578127+fazekas28@users.noreply.github.com>
2024-01-16 14:23:50 -0300

    Update SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -27,7 +27,7 @@ export default function SideBar() {
     <a href='/dashboard' className='text-white p-4 hover:bg-gray-700'>
      <FaChartLine size={20} />
     </a>
-    <a href='search' className='text-white p-4 hover:bg-gray-700'>
+    <a href='/search' className='text-white p-4 hover:bg-gray-700'>
      <FaSearch size={20} />
     </a>
     <a href='/filialinfo' className='text-white p-4 hover:bg-gray-700'>
