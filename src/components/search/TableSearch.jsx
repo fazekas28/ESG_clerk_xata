5edef3581166785f9268bf6128f43dc4037a5de1
fazekas28 <148578127+fazekas28@users.noreply.github.com>
2024-01-16 16:48:19 -0300

    Create TableSearch.jsx

---
+++ src/components/search/TableSearch.jsx
@@ -0,0 +1,40 @@
+import { UserButton, auth } from '@clerk/nextjs';
+import React from 'react'
+import { FaHome, FaFolder, FaSearch, FaCog, FaPaperPlane, FaChartLine } from 'react-icons/fa';
+
+
+export default function SideBar() {
+
+ const { userId } = auth();
+
+ if (!userId) {
+  return null;
+ }
+
+ return (
+  <div className='bg-gray-800 h-full w-16 fixed top-0 left-0 flex flex-col items-center justify-between pt-20'>
+   <div className='flex flex-col items-center pt-8'>
+    <div className=' flex w-full h-16 items-center justify-center hover:bg-gray-700'>
+     <UserButton />
+    </div>
+
+    <a href='/' className='text-white p-4 hover:bg-gray-700'>
+     <FaHome size={20} />
+    </a>
+    <a href='/sendmtrinfo' className='text-white p-4 hover:bg-gray-700'>
+     <FaPaperPlane size={20} />
+    </a>
+    <a href='/dashboard' className='text-white p-4 hover:bg-gray-700'>
+     <FaChartLine size={20} />
+    </a>
+    <a href='/search' className='text-white p-4 hover:bg-gray-700'>
+     <FaSearch size={20} />
+    </a>
+    <a href='/filialinfo' className='text-white p-4 hover:bg-gray-700'>
+     <FaCog size={20} />
+    </a>
+
+   </div>
+  </div >
+ )
+}
