5ca85bf5eb7bd972a7f98ab1d327bb812535ebd4
fazekas28 <148578127+fazekas28@users.noreply.github.com>
2024-01-16 16:55:09 -0300

    Update TableSearch.jsx

--- src/components/search/TableSearch.jsx
+++ src/components/search/TableSearch.jsx
@@ -1,40 +1,139 @@
-import { UserButton, auth } from '@clerk/nextjs';
-import React from 'react'
-import { FaHome, FaFolder, FaSearch, FaCog, FaPaperPlane, FaChartLine } from 'react-icons/fa';
+'use client'
+import React, { useState } from 'react'
+import { RiSearchLine } from 'react-icons/ri'
+import { Table } from 'flowbite-react'
+import { FaFileCsv } from 'react-icons/fa'
+
+export default function TableSearch({ fetchDataTable }) {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [data, setData] = useState([]);
+
+  const handleStartDateChange = (event) => {
+    setStartDate(new Date(event.target.value));
+  };
+
+  const handleEndDateChange = (event) => {
+    setEndDate(new Date(event.target.value));
+  };
+
+  const handleSearch = async () => {
+    const res = await fetchDataTable(startDate, endDate);
+    setData(res)
+  };
 
 
-export default function SideBar() {
+  function arrayToCSV(data) {
+    if (!data.length) {
+      return '';
+    }
 
- const { userId } = auth();
+    // Define the desired order of columns
+    const columnOrder = [
+      'data',
+      'link_id.filial',
+      'link_id.cnpj_filial',
+      'link_id.transp',
+      'link_id.cnpj_transp',
+      'link_id.dest',
+      'link_id.cnpj_dest',
+      'link_id.tratamento',
+      'link_id.uf',
+      'link_id.und',
+      'n_mtr',
+      'n_cdf',
+      'n_nf',
+      'chave_nf',
+      'volume',
+    ];
+    const rows = data.map(record => columnOrder.map(column => {
+      const nestedProps = column.split('.');
+      return nestedProps.reduce((obj, prop) => obj[prop], record);
+    }));
+    const csv = '\ufeff' + [columnOrder.join(';')].concat(rows.map(row => row.join(';'))).join('\n');
+    return csv;
+  }
 
- if (!userId) {
-  return null;
- }
+  function downloadCSV(data) {
+    const csvContent = arrayToCSV(data);
+    const blob = new Blob([csvContent], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
 
- return (
-  <div className='bg-gray-800 h-full w-16 fixed top-0 left-0 flex flex-col items-center justify-between pt-20'>
-   <div className='flex flex-col items-center pt-8'>
-    <div className=' flex w-full h-16 items-center justify-center hover:bg-gray-700'>
-     <UserButton />
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'data.csv';
+    a.click();
+
+    URL.revokeObjectURL(url);
+    setData([])
+  }
+
+
+
+  return (
+    <div>
+      <div className="flex space-x-4 items-center">
+        <label htmlFor="startDate" className="text-gray-600">Data Inicial:</label>
+        <input
+          type="date"
+          id="startDate"
+          value={startDate.toISOString().split('T')[0]}
+          onChange={handleStartDateChange}
+          className="border rounded p-2 focus:outline-none focus:border-blue-500"
+        />
+
+        <label htmlFor="endDate" className="text-gray-600">Data Final:</label>
+        <input
+          type="date"
+          id="endDate"
+          value={endDate.toISOString().split('T')[0]}
+          onChange={handleEndDateChange}
+          className="border rounded p-2 focus:outline-none focus:border-blue-500"
+        />
+
+        <button
+          onClick={handleSearch}
+          className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded focus:outline-none"
+        >
+          <RiSearchLine className="inline-block mr-2" />
+          Search
+        </button>
+
+        {data.length > 0 && (
+          <button
+            onClick={() => downloadCSV(data)}
+            className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded focus:outline-none"
+          >
+            <FaFileCsv className="inline-block mr-2" />
+            Download
+          </button>
+        )}
+      </div>
+      <div className="pt-5">
+        <Table striped>
+          <Table.Head>
+            <Table.HeadCell className='bg-blue-800 text-white font-medium'>Loja</Table.HeadCell>
+            <Table.HeadCell className='bg-blue-800 text-white font-medium'>COD_IBAMA</Table.HeadCell>
+            <Table.HeadCell className='bg-blue-800 text-white font-medium'>MTR</Table.HeadCell>
+            <Table.HeadCell className='bg-blue-800 text-white font-medium'>CDF</Table.HeadCell>
+            <Table.HeadCell className='bg-blue-800 text-white font-medium'>NF</Table.HeadCell>
+            <Table.HeadCell className='bg-blue-800 text-white font-medium'>VOLUME</Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {data.map((record) => (
+              <Table.Row key={record.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className='text-center'>{record.link_id.filial}</Table.Cell>
+                <Table.Cell className='text-center'>{record.link_id.cod_ibama}</Table.Cell>
+                <Table.Cell className='text-center'>{record.n_mtr}</Table.Cell>
+                <Table.Cell className='text-center'>{record.n_cdf}</Table.Cell>
+                <Table.Cell className='text-center'>{record.n_nf}</Table.Cell>
+                <Table.Cell className='text-center'>{record.volume}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+
+      </div>
     </div>
-
-    <a href='/' className='text-white p-4 hover:bg-gray-700'>
-     <FaHome size={20} />
-    </a>
-    <a href='/sendmtrinfo' className='text-white p-4 hover:bg-gray-700'>
-     <FaPaperPlane size={20} />
-    </a>
-    <a href='/dashboard' className='text-white p-4 hover:bg-gray-700'>
-     <FaChartLine size={20} />
-    </a>
-    <a href='/search' className='text-white p-4 hover:bg-gray-700'>
-     <FaSearch size={20} />
-    </a>
-    <a href='/filialinfo' className='text-white p-4 hover:bg-gray-700'>
-     <FaCog size={20} />
-    </a>
-
-   </div>
-  </div >
- )
+  );
 }
