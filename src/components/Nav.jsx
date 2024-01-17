002a6888b8cac6a2c8d2ee73bf0e28f0d5d2d076
fazekas28 <148578127+fazekas28@users.noreply.github.com>
2024-01-17 15:04:26 -0300

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { OrganizationSwitcher } from '@clerk/nextjs'
+import React from 'react';
+import { OrganizationSwitcher } from '@clerk/nextjs';
 
 export default function Header() {
-
  return (
   <header>
    <nav className="bg-gray-800">
-    <div className="px-2 sm:px-6 lg:px-8">
-     <div className="relative flex h-16 items-center justify-between">
+    <div className="sm:px-6 lg:px-8">
+     <div className="relative flex h-10 sm:h-14 items-center justify-between">
 
       <div className="flex flex-1 items-center sm:items-stretch sm:justify-start">
 
-       <div>
-        <a className='text-white font-bold lg:tracking-[0.25em] pl-16 lg:pl-10'>Pateo Hyundai ESG</a>
+       <div className=''>
+        <a className='text-white font-bold lg:tracking-[0.25em] pl-10 lg:pl-10'>Pateo Hyundai ESG</a>
        </div>
 
       </div>
-      <div className=' flex flex-row gap-5'>
+
+      <div className='flex flex-row gap-5'>
        <div>
         <OrganizationSwitcher />
        </div>
@@ -26,7 +26,6 @@ export default function Header() {
     </div>
 
    </nav>
-
   </header>
- )
+ );
 }
