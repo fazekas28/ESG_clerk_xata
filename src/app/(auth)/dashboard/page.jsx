816bcd9605614c1825b7268b08d3fcc3a73872c5
fazekas28 <148578127+fazekas28@users.noreply.github.com>
2024-01-18 16:31:18 -0300

    Update page.jsx

--- src/app/(auth)/dashboard/page.jsx
+++ src/app/(auth)/dashboard/page.jsx
@@ -1,33 +1,78 @@
 import React from 'react';
 import Cards from '../../../components/dashboard/Cards';
-import DashboardComp from '../../../components/dashboard/DashboardComp'
 import { getXataClient } from '../../../lib/xata';
-import { sumVolumesByMonth } from '../../../utils/sumVolumesByMonth';
-import { sumVolumesByMonthAndIbama } from '../../../utils/sumVolumesByMonthAndIbama';
-import { sumVolumesByFilial } from '../../../utils/sumVolumesByFilial';
 import { auth } from '@clerk/nextjs';
-import processChartData from '../../../utils/barChartData'
-import processMonthlyData from '../../../utils/barChartDataMonth'
 
 const xata = getXataClient();
 
 export default async function Dashboard() {
  const { sessionClaims } = auth();
  const filial_name = sessionClaims?.org_slug;
+ const role = sessionClaims?.org_role;
 
- const data = await xata.db.mtr_info.select(["*", "link_id.filial", "link_id.cod_ibama"]).getAll();
- const result = sumVolumesByMonth(data);
- const result2 = sumVolumesByMonthAndIbama(data, null);
- const result3 = sumVolumesByFilial(data, null, filial_name);
- const loja = filial_name;
- const barChart = processChartData(data)
- const barChart2 = processMonthlyData(loja, data)
+ const groups = [
+  [{ 'link_id.filial': 'pateo-ilha' }, { 'link_id.filial': 'pateo-retiro' }, { 'link_id.filial': 'pateo-holandeses' }, { 'link_id.filial': 'pateo-joao-pessoa' }],
+  [{ 'link_id.filial': 'pateo-manaus' }, { 'link_id.filial': 'pateo-feira' }, { 'link_id.filial': 'pateo-olinda' }],
+  [{ 'link_id.filial': 'pateo-bequimao' }, { 'link_id.filial': 'pateo-piedade' }]
+ ];
+
+ const matchingGroup = groups.find(group => group.some(filial => filial["link_id.filial"] === filial_name));
+
+
+ const fetchDataChart = async (startDate, endDate) => {
+  'use server'
+
+  const records = await xata.db.mtr_info
+   .filter({
+    data: {
+     $ge: startDate,
+     $le: endDate,
+    },
+    "link_id.filial": filial_name,
+   })
+   .summarize({
+    columns: ["link_id.cod_ibama"],
+    summaries: {
+     total_volume: { sum: "volume" },
+     total_records: { 'count': '*' },
+    },
+   });
+
+  const recordsJSON = JSON.parse(JSON.stringify(records));
+
+  return recordsJSON
+
+ }
+
+ const fetchBarChartData = async (startDate, endDate) => {
+  'use server'
+
+  const records = await xata.db.mtr_info
+   .filter({
+    data: {
+     $ge: startDate,
+     $le: endDate,
+    },
+    $any: matchingGroup,
+   }).summarize({
+    columns: ["link_id.cod_ibama", 'link_id.filial'],
+    summaries: {
+     total_volume: { sum: "volume" },
+     total_records: { count: "*" },
+    },
+   });
+
+
+  const recordsJSON = JSON.parse(JSON.stringify(records));
+
+  return recordsJSON
+
+ }
 
 
  return (
   <div className='flex flex-col pl-[4.25em] pt-10 gap-5'>
-   <Cards sum={result} oleo={result2} filial={result3} pateo={loja} />
-   <DashboardComp chartdata={barChart} chartdata2={barChart2} pateo={loja} />
+   <Cards fetchDataChart={fetchDataChart} fetchBarChartData={fetchBarChartData} />
   </div>
  );
 }
