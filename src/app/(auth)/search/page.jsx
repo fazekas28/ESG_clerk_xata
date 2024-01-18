309ed2b17f26add4538b016a9ba3e4bcee911cff
fazekas28 <148578127+fazekas28@users.noreply.github.com>
2024-01-18 09:27:00 -0300

    Update page.jsx

--- src/app/(auth)/search/page.jsx
+++ src/app/(auth)/search/page.jsx
@@ -1,39 +1,84 @@
 import React from 'react'
 import TableSearch from '../../../components/search/TableSearch'
 import { getXataClient } from '../../../lib/xata'
+import { auth } from '@clerk/nextjs';
+
 
 const xata = getXataClient();
 
 export default function Search() {
+ const { userId, sessionClaims } = auth();
+
+ const userRole = sessionClaims?.org_role
+ const userFilial = sessionClaims?.org_slug
 
  const fetchDataTable = async (startDate, endDate) => {
   'use server'
-  const records = await xata.db.mtr_info
-   .filter({
-    data: {
-     $ge: startDate,
-     $le: endDate,
-    },
-   })
-   .select([
-    "*",
-    "link_id.cod_uni",
-    "link_id.filial",
-    "link_id.cod_ibama",
-    "link_id.cnpj_dest",
-    "link_id.cnpj_filial",
-    "link_id.cnpj_transp",
-    "link_id.dest",
-    "link_id.transp",
-    "link_id.tratamento",
-    "link_id.uf",
-    "link_id.und",
-   ])
-   .getMany();
 
-  const recordsJSON = JSON.parse(JSON.stringify(records));
+  if (userRole === 'org:admin') {
+   const records = await xata.db.mtr_info
+    .filter({
+     data: {
+      $ge: startDate,
+      $le: endDate,
+     },
+    })
+    .select([
+     "*",
+     "link_id.cod_uni",
+     "link_id.filial",
+     "link_id.cod_ibama",
+     "link_id.cnpj_dest",
+     "link_id.cnpj_filial",
+     "link_id.cnpj_transp",
+     "link_id.dest",
+     "link_id.transp",
+     "link_id.tratamento",
+     "link_id.uf",
+     "link_id.und",
+     "link_id.sistema"
+    ])
+    .getMany();
 
-  return recordsJSON;
+   const recordsJSON = JSON.parse(JSON.stringify(records));
+
+   return recordsJSON;
+
+  } else {
+
+   const records = await xata.db.mtr_info
+    .filter({
+     data: {
+      $ge: startDate,
+      $le: endDate,
+     },
+     'link_id.filial': userFilial,
+    })
+    .select([
+     "*",
+     "link_id.cod_uni",
+     "link_id.filial",
+     "link_id.cod_ibama",
+     "link_id.cnpj_dest",
+     "link_id.cnpj_filial",
+     "link_id.cnpj_transp",
+     "link_id.dest",
+     "link_id.transp",
+     "link_id.tratamento",
+     "link_id.uf",
+     "link_id.und",
+     "link_id.sistema",
+    ])
+    .getMany();
+
+   const recordsJSON = JSON.parse(JSON.stringify(records));
+
+   return recordsJSON;
+
+
+  }
+
+
 
  }
 
