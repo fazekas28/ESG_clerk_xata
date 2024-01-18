c5bcc855b5c379ecfa062638e4acb2f7c079efb4
fazekas28 <148578127+fazekas28@users.noreply.github.com>
2024-01-18 08:54:21 -0300

    Update page.jsx

--- src/app/(auth)/filialinfo/page.jsx
+++ src/app/(auth)/filialinfo/page.jsx
@@ -1,54 +1,63 @@
 import React from 'react'
-import Filial_info_form from '../../../components/form/Filial_info_form';
-import { getXataClient } from '../../../lib/xata';
+import Send_mtr_form from '../../../components/form/Send_mtr_form'
+import { getXataClient } from '../../../lib/xata'
 import { auth } from '@clerk/nextjs';
 import { Toaster } from 'react-hot-toast';
 
+const xata = getXataClient()
 
-const xata = getXataClient();
-
-export default async function FilialInfo() {
-  const { sessionClaims } = auth();
-
-  const role = sessionClaims?.org_role
+export default async function SendMtrInfo() {
+  const { sessionClaims, userId } = auth();
   const filial_name = sessionClaims?.org_slug
 
-  const fetchData = async (cod) => {
+  const getFilialInfo = async (cod) => {
     'use server'
     const cod_ibama = parseInt(cod)
-    const info = await xata.db.filial_info.filter({
+    const filialInfo = await xata.db.filial_info.filter({
       filial: filial_name,
-      cod_ibama: cod_ibama,
+      cod_ibama: cod_ibama
     }).getMany();
 
-
-    const data = JSON.parse(JSON.stringify(info))
-    return data
-  };
-
-  const editData = async (formData, id) => {
-    'use server'
-    try {
-      const updatedFilial = await xata.db.filial_info.update(id, {
-        cnpj_filial: formData.cnpj_filial,
-        cnpj_transp: formData.cnpj_transp,
-        cnpj_dest: formData.cnpj_dest,
-        tratamento: formData.tratamento,
-        uf: formData.uf,
-        und: formData.und
-      });
-    } catch (error) {
-      return error
-    }
-
-    return "Dados editados com sucesso!"
-
+    if (!filialInfo) {
+      return "aconteceu algum erro!"
+    } else return filialInfo[0].id
 
   }
 
+  const saveMtrInfo = async (formData, id) => {
+    'use server'
+
+    const inputDate = new Date(formData.data);
+    const formatedDate = inputDate.toISOString();
+
+    const volumeFloat = parseFloat(formData.volume)
+
+    try {
+      const saveMtrInfo = await xata.db.mtr_info.create({
+        link_id: id,
+        data: formatedDate,
+        volume: volumeFloat,
+        n_mtr: formData.n_mtr,
+        n_cdf: formData.n_cdf,
+        n_nf: formData.n_nf,
+        chave_nf: formData.chave_nf,
+        userId: userId
+      })
+
+
+    } catch (error) {
+      return error
+    }
+    return 'Informações salvas com sucesso!'
+
+  }
+
+
+
+
   return (
     <div className='flex flex-col pl-[3.80em] pt-10 gap-5'>
-      <Filial_info_form fetchData={fetchData} editData={editData} />
+      <Send_mtr_form getFilialInfo={getFilialInfo} saveMtrInfo={saveMtrInfo} />
       <Toaster
         toastOptions={{
           duration: 4000
