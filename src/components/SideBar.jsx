b1d49c0ccd646a05abef3086f76ee111880f6eef
fazekas28 <148578127+fazekas28@users.noreply.github.com>
2024-04-30 12:21:33 -0300

    Update SideBar.jsx
    <UserButton afterSignOutUrl='/' />

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -13,7 +13,7 @@ export default function SideBar() {
   <div className='bg-gray-800 h-full w-10 fixed top-0 left-0 flex flex-col items-center justify-between pt-20 sm:w-14'>
    <div className='flex flex-col items-center pt-8 gap-2'>
     <div className=' flex w-full h-12 items-center justify-center hover:bg-gray-700'>
-     <UserButton />
+     <UserButton afterSignOutUrl='/' />
     </div>
 
     <a href='/' className='text-white p-2 hover:bg-gray-700'>
