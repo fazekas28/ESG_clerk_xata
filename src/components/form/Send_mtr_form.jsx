2086712a7cc8965572afd199e6b3659e4579b12a
fazekas28 <148578127+fazekas28@users.noreply.github.com>
2024-05-15 10:58:20 -0300

    Update Send_mtr_form.jsx

--- src/components/form/Send_mtr_form.jsx
+++ src/components/form/Send_mtr_form.jsx
@@ -167,7 +167,6 @@ export default function SendMtrForm({ getFilialInfo, saveMtrInfo }) {
         value={formData.n_nf}
         onChange={handleInputChange}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 uppercase"
-        required
        />
       </div>
       <div>
@@ -180,7 +179,6 @@ export default function SendMtrForm({ getFilialInfo, saveMtrInfo }) {
         value={formData.chave_nf}
         onChange={handleInputChange}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 uppercase"
-        required
        />
       </div>
 
