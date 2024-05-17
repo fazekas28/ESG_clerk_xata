5a93255e71e3795b15c970609ee83699d273f57d
fazekas28 <148578127+fazekas28@users.noreply.github.com>
2024-05-17 10:10:56 -0300

    Update page.jsx
    update para aceitar valores null para nf e chave nf

--- src/app/(auth)/sendmtrinfo/page.jsx
+++ src/app/(auth)/sendmtrinfo/page.jsx
@@ -11,47 +11,47 @@ export default async function SendMtrInfo() {
   const filial_name = sessionClaims?.org_slug
 
   const getFilialInfo = async (cod) => {
-    'use server'
-    const cod_ibama = parseInt(cod)
+    'use server';
+    const cod_ibama = parseInt(cod);
     const filialInfo = await xata.db.filial_info.filter({
       filial: filial_name,
       cod_ibama: cod_ibama
     }).getMany();
-
-    if (!filialInfo) {
-      return "aconteceu algum erro!"
-    } else return filialInfo[0].id
-
-  }
+  
+    if (!filialInfo || filialInfo.length === 0) {
+      return { error: "aconteceu algum erro!" }; 
+    }
+  
+    return { id: filialInfo[0].id }; 
+  };
 
   const saveMtrInfo = async (formData, id) => {
-    'use server'
-
+    'use server';
+  
     const inputDate = new Date(formData.data);
     const formatedDate = inputDate.toISOString();
-
-    const volumeFloat = parseFloat(formData.volume)
-
+    const volumeFloat = parseFloat(formData.volume);
+  
+    const chave_nf = formData.chave_nf.trim() === '' ? '0' : formData.chave_nf;
+    const n_nf = formData.n_nf.trim() === '' ? '0' : formData.n_nf;
+  
     try {
-      const saveMtrInfo = await xata.db.mtr_info.create({
+      await xata.db.mtr_info.create({
         link_id: id,
         data: formatedDate,
         volume: volumeFloat,
         n_mtr: formData.n_mtr,
         n_cdf: formData.n_cdf,
-        n_nf: formData.n_nf,
-        chave_nf: formData.chave_nf,
+        n_nf: n_nf,
+        chave_nf: chave_nf, 
         userId: userId
-      })
-
-
+      });
+  
+      return { message: 'Informações salvas com sucesso!' }; 
     } catch (error) {
-      return error
+      return { error: error.message }; 
     }
-    return 'Informações salvas com sucesso!'
-
-  }
-
+  };
 
 
 
